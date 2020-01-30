6c44db8415f2aeaa0ae739f356149d2c9d117e61
Phill Haines <omnibitjp@gmail.com>
2020-01-30 20:18:47 +0900

    Updates 30/01
    Added Lives int but need to make this integer work outside of scope

--- PassCrack.cpp
+++ PassCrack.cpp
@@ -4,30 +4,34 @@
 void PrintIntroduction(int Difficulty)
 {
     // print introduction messages to terminal
-    std::cout << "Today is the day! Maybe I can finally get my promotion with flawless performance.";
+    std::cout << "\nToday is the day! Maybe I can finally get my promotion.";
     std::cout << "\nAs always with the start of the day I need to start at the bottom at level " << Difficulty << " password solving.";
     std::cout << "\nAh, here comes the call!\n";
 }
 
 bool PlayGame(int Difficulty)
 {
+    if (Difficulty == 1)
+    {
     PrintIntroduction(Difficulty);
+    }
     
     // Declare a 3 number code
     int CodeA = rand() % Difficulty + Difficulty, CodeB = rand() % Difficulty + Difficulty, CodeC = rand() % Difficulty + Difficulty;
 
     int CodeSum = CodeA + CodeB + CodeC;
     int CodeProduct = CodeA * CodeB * CodeC;
+    int Lives = 3;
     
     // print sum and product to terminal
-    std::cout << "There are 3 numbers in each password";
+    std::cout << "\nThere are 3 numbers in each password";
     std::cout << "\nThe three codes add-up to: " << CodeSum;
     std::cout << "\nThe codes multiply to give: " << CodeProduct;
 
     int PlayerGuess;
     int GuessA, GuessB, GuessC;
 
-    std::cout << "\nPlease enter your guess below using a space between each digit e.g X X X.\n\n";
+    std::cout << "\n\nPlease enter your guess below using a space between each digit e.g X X X.\n\n";
     std::cin >> GuessA;
     std::cin >> GuessB;
     std::cin >> GuessC;
@@ -37,12 +41,13 @@ bool PlayGame(int Difficulty)
 
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "\n*** Nice job but here comes the next call! ***\n";
+        std::cout << "\n*** Great work. You are correct. Time to take it up a notch with this next call! ***\n";
         return true;
     }
     else 
     {
-        std::cout << "\n*** That is not the correct password, try again! ***\n";
+        --Lives //remove one life
+        std::cout << "\n*** That is not the correct password but no time to waste. Here comes the next call! ***\n";
         return false;
     }
 }
@@ -54,7 +59,7 @@ int main()
     int LevelDifficulty = 1;
     int const MaxDifficulty = 5;
     
-     while (LevelDifficulty <= MaxDifficulty) //Loop game until max level is beaten
+     while (LevelDifficulty <= MaxDifficulty && Lives > 0) //Loop game until max level is beaten or lives run out
     {
 
         bool bLevelComplete = PlayGame(LevelDifficulty);
